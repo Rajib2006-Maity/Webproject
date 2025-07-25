2a24b70581d58336110533dca09748a8596b00d4
Rajib2006-Maity <rajibmaity016@gmail.com>
2025-07-26 00:10:21 +0530

    Add files via upload

---
+++ assignment_9.c
@@ -0,0 +1,82 @@
+/*#include<stdio.h>
+int main()
+{
+    int cp,sp,x;
+    printf("enter cost price");
+    scanf("%f",&cp);
+    printf("enter selling price");
+    scanf("%f",&sp);
+    if(sp>=cp)
+    {
+        x=(sp-cp)/cp*100;
+        printf("profit: %0.2f%%",x);
+    }
+    else
+    {
+        x=(cp-sp)/cp*100;
+        printf("loss:%0.2f%%",x);
+    }
+    return 0;
+    
+}*/
+/*#include <stdio.h>
+int main()
+{
+    int m1,m2,m3,m4,m5;
+    printf("enter marks of 5 subjects");
+    scanf("%d%d%d%d%d",&m1,&m2,&m3,&m4,&m5);
+    if(m1>=33 && m2>=33 && m3>=33 && m4>=33 && m5>=33)
+    {
+        printf("result:pass");
+    }
+    else{
+        printf("result:fail");
+    }
+    printf("\n");
+    return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter an alphabet");
+    scanf("%c",&ch);
+    if(ch>='a'&& ch<='z')
+    printf("lowercase alphabet");
+    else if(ch>='A'&& ch<='z')
+    printf("uppercase alphabet");
+    else
+    printf("not an alphabet");
+    printf("\n");
+    return 0;
+}*/
+/*#include <stdio.h>
+int main()
+{
+    int x;
+    printf("enter a number");
+    scanf("%d",&x);
+    if(x%3==0 && x%2==0)
+    printf("it is divisible by 3 and 2");
+    else
+    printf("it is not divisible by both 3 and 2");
+    printf("\n");
+    return 0;
+}*/
+/*#include <stdio.h>
+int main()
+{
+    int x;
+    printf("enter a number");
+    scanf("%d",&x);
+    if(x%7==0 || x%3==0)
+    printf("it is divisible by 7 and 3");
+    else
+    printf("it is not divisible by both 7 and 3");
+    printf("\n");
+    return 0;
+}*/
+ 
+
+
+
