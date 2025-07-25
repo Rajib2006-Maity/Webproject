9019e62c61945287d89aaf686f417016a80a36d1
Rajib2006-Maity <rajibmaity016@gmail.com>
2025-07-26 00:06:49 +0530

    Add files via upload

---
+++ assignment_11.c
@@ -0,0 +1,111 @@
+/*#include <stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=5)
+    {
+        printf("MySirG\n");
+        i++;
+    }
+    return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10){
+    printf("%d ",i);
+    i++;
+}
+return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int i=10;
+    while (i>=1){
+    printf("%d ",i);
+    i--;
+}
+return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10){
+    printf("%d ",2*i-1);
+    i++;
+}
+return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10){
+    printf("%d ",21-2*i);
+    i++;
+}
+return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10){
+    printf("%d ",2*i);
+    i++;
+}
+return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10){
+    printf("%d ",22-2*i);
+    i++;
+}
+return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10){
+    printf("%d ",i*i);
+    i++;
+}
+return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10){
+    printf("%d ",i*i*i);
+    i++;
+}
+return 0;
+}*/
+/*#include <stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10){
+        printf("5 × %d = %d\n",i,5*i);
+        i++;
+    }
+    return 0;
+}*/
+
+
+
+
+
+    
+    
+
+
+
